d7bb7d10ad80308e6d325e4dd312a4f30d2c0c4c
shamikhan005 <shamikhan8595@gmail.com>
2025-02-06 20:30:35 +0530

    added new protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,55 @@
 import { NextResponse, NextRequest } from "next/server";
+import { createClient } from "@supabase/supabase-js";
 
-export function middleware(request: NextRequest) {
-  const accessToken = request.cookies.get('sb-access-token')
-  const path = request.nextUrl.pathname
+export async function middleware(request: NextRequest) {
 
-  const protectedRoutes = ['/dashboard']
+  const path = request.nextUrl.pathname;
 
-  if (protectedRoutes.includes(path)) {
+  if (path.startsWith('/dashboard')) {
+    const accessToken = request.cookies.get('sb-access-token');
     if (!accessToken) {
-      return NextResponse.redirect(new URL('/login', request.url))
+      return NextResponse.redirect(new URL('/login', request.url));
     }
   }
-  return NextResponse.next()
+
+  if (path.startsWith('/api/webhooks')) {
+    const authHeader = request.headers.get('Authorization');
+    if (!authHeader) {
+      return NextResponse.json( { error: 'unauthorized - no token provided' }, { status: 401 });
+    }
+
+    try {
+      const supabase = createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      )
+
+      const token = authHeader.replace('Bearer ', '');
+      const { data: { user }, error } = await supabase.auth.getUser(token);
+      
+      if (error || !user) {
+        return NextResponse.json( { error: 'invalid token' }, { status: 401 });
+      }
+    } catch (error) {
+      return NextResponse.json( { error: 'invalid token' }, { status: 401 })
+    }
+  }
+
+  return NextResponse.next();
+
+  // const accessToken = request.cookies.get('sb-access-token')
+  // const path = request.nextUrl.pathname
+
+  // const protectedRoutes = ['/dashboard']
+
+  // if (protectedRoutes.includes(path)) {
+  //   if (!accessToken) {
+  //     return NextResponse.redirect(new URL('/login', request.url))
+  //   }
+  // }
+  // return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/dashboard']
+  matcher: ['/dashboard', '/api/webhooks/:path*']
 }
